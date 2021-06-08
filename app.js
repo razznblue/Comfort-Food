8f71004e5a82f19d85bc3e531be0b87a3a5bb912
KaipoJames <kaiposemail@yahoo.com>
2021-06-08 15:40:04 -0700

    render HTML page to server with style sheet linked

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3005;
+
+const variables = {
+    username: "driloves"
+}
+
+// Implement ejs tamplate engine to render html file from views folder
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Tell our app to look in the styles folder for styling
+app.use(express.static("public/styles"));
+
+app.get("/", (req, res) => {
+    res.render("index", variables);
+});
+app.get("/home", (req, res) => {
+    res.send("Welcome To The Home Page!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
